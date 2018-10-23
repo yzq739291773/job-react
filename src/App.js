bba16752ae0c790b5f380af2014e49626cecb2f1
yzq <739291773@qq.com>
2018-10-23 13:15:43 +0800

    用react-redux连接react和redux

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
+
+import { counter, addGun, removeGun, addGunAsync } from './index.redux'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    const store = this.props.store
-    const addGun = this.props.addGun
-    const removeGun = this.props.removeGun
-    const addGunAsync = this.props.addGunAsync
-    const num = store.getState()
-    console.log('app的props',this.props)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>当前武器数量{num}</h1>
-          <button onClick={()=>{store.dispatch(addGun())}}>申请武器</button>
-          <button onClick={()=>{store.dispatch(removeGun())}}>上交武器</button>
-          <button onClick={()=>{store.dispatch(addGunAsync())}}>拖两天再给</button>
+          <h1>当前武器数量{this.props.num}</h1>
+          <button onClick={this.props.addGun}>申请武器</button>
+          <button onClick={this.props.removeGun}>上交武器</button>
+          <button onClick={this.props.addGunAsync}>拖两天再给</button>
         </header>
       </div>
     );
   }
 }
-
+const mapStatetoProps = (state)=>{
+  return {num:state}
+}
+const actionCreaters = {counter, addGun, removeGun, addGunAsync}
+App = connect(mapStatetoProps, actionCreaters)(App)
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,29 +6,43 @@ import * as serviceWorker from './serviceWorker';
 // import demo_redux from './demo/demo-redux.js'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import { counter, addGun, removeGun, addGunAsync } from './index.redux'
+import { Provider } from 'react-redux'
+// import { addGun, removeGun, addGunAsync } from './index.redux'
+import { counter } from './index.redux'
 
 const store = createStore(counter, compose(
     applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f
 ))
 
+// 1.react 原始代码
+
 { /* ReactDOM.render( < App / > , document.getElementById('root')); */ }
 
-function render() {
-    ReactDOM.render( <
-        App store = { store }
-        addGun = { addGun }
-        removeGun = { removeGun }
-        addGunAsync = { addGunAsync }
-        / > , document.getElementById('root'));
-    }
+// 2.手动连接react和redux
 
-    render()
-    store.subscribe(render)
+// function render() {
+//     ReactDOM.render( <
+//         App store = { store }
+//         addGun = { addGun }
+//         removeGun = { removeGun }
+//         addGunAsync = { addGunAsync }
+//         / > , document.getElementById('root'));
+//     }
 
+//     render()
+//     store.subscribe(render)
 
-    // If you want your app to work offline and load faster, you can change
-    // unregister() to register() below. Note this comes with some pitfalls.
-    // Learn more about service workers: http://bit.ly/CRA-PWA
-    serviceWorker.unregister();
+// 3.利用react-redux插件连接redux和react
+ReactDOM.render( <
+    Provider store = { store } >
+    <
+    App / >
+    <
+    /Provider>, document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
