6ea84c5ffc2122eaaedf0234f3d48b2de2f29cb3
yzq <739291773@qq.com>
2018-10-23 10:22:59 +0800

    redux的api简单使用

---
+++ src/demo/demo-redux.js
@@ -0,0 +1,43 @@
+import { createStore } from 'redux'
+
+const ADD_GUN = '加机关枪'
+const REMOVE_GUN = '减机关枪'
+
+// reducer
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case ADD_GUN:
+            return state + 1;
+        case REMOVE_GUN:
+            return state - 1;
+        default:
+            return 10;
+    }
+}
+
+const store = createStore(counter)
+
+const init = store.getState()
+console.log('初始机枪数量', init)
+
+
+const listener = () => {
+    const current = store.getState()
+    console.log('当前机枪数量', current)
+}
+
+
+// action creater
+const addGun = () => {
+    return { type: ADD_GUN }
+}
+const removeGun = () => {
+    return { type: REMOVE_GUN }
+}
+
+store.subscribe(listener)
+
+
+store.dispatch(addGun())
+store.dispatch(addGun())
+store.dispatch(removeGun())
