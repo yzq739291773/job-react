0999fdef46b5533de615b4c09869b745062cd1fc
yzq <739291773@qq.com>
2018-10-30 11:18:59 +0800

    添加登录注册页的表单

--- src/container/login/login.jsx
+++ src/container/login/login.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import { Button, WhiteSpace, WingBlank, List, InputItem } from 'antd-mobile';
 import Logo from '../../component/logo/logo'
 
 class Login extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+    register(){
+        console.log('注册',this.props)
+        this.props.history.push('/register');
+    }
     render(){
         return(
             <div>
                 <Logo></Logo>
-                <h2>登陆页</h2>
+                <WingBlank>
+                    <List>
+                        <InputItem>用户</InputItem>
+                        <WhiteSpace />
+                        <InputItem>密码</InputItem>
+                        <WhiteSpace />
+                    </List>
+                    <Button type="primary">登录</Button>
+                    <WhiteSpace />
+                    <Button onClick={()=>this.register()} type="primary">注册</Button>
+                    <WhiteSpace />
+                </WingBlank>
             </div>
         )
     }
--- src/container/register/register.jsx
+++ src/container/register/register.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
+import { Button, WhiteSpace, WingBlank, List, InputItem, Radio } from 'antd-mobile';
 import Logo from '../../component/logo/logo'
 
 class Register extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+    handRegister(){
+        console.log('注册成功')
+    }
     render(){
+        const RadioItem = Radio.RadioItem;
         return(
             <div>
                 <Logo></Logo>
-                <h2>注册页</h2>
+                <WingBlank>
+                    <List>
+                        <InputItem>用户</InputItem>
+                        <WhiteSpace />
+                        <InputItem>密码</InputItem>
+                        <WhiteSpace />
+                        <InputItem>确认密码</InputItem>
+                        <WhiteSpace />
+                        <RadioItem>
+                            牛人
+                        </RadioItem>
+                        <WhiteSpace />
+                        <RadioItem>
+                            boss
+                        </RadioItem>
+                    </List>
+                    <Button onClick={()=>this.handRegister()} type="primary">注册</Button>
+                    <WhiteSpace />
+                </WingBlank>
             </div>
         )
     }
