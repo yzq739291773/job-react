6353e3dd2ed80ae3adf8aa06bc133d5fbc81fba3
yzq <739291773@qq.com>
2018-11-02 14:40:06 +0800

    完成bossinfo 和头像选择静态页面的编写

---
+++ src/component/avatar-selector/avatar-selector.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {Grid, List} from 'antd-mobile'
+
+class AvatarSelector extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            icon:'',
+            text:''
+        }
+    }
+    
+    render(){
+        const avatarList = 'boy,girl,man,woman,bull,chick,crab,hedgehog,hippopotamus,koala,lemur,pig,tiger,whale,zebra'
+												.split(',')
+												.map(v=>({
+													icon:require(`../img/${v}.png`),
+													text:v
+												}))
+		const gridHeader = this.state.icon
+												? (<div>
+														<span>已选择头像</span>
+														<img style={{width:20}} src={this.state.icon} alt=""/>
+													</div>)
+												: '请选择头像'
+        return(
+            <div>
+                <List renderHeader={()=>gridHeader}>
+					<Grid
+						data={avatarList}
+						columnNum={5} 
+						onClick={elm=>{
+                            console.log(11,elm)
+                            this.setState(elm)
+						}}
+					/>					
+				</List>
+            </div>
+        )
+    }
+}
+
+export default AvatarSelector
